b4c71d1a9298b34bcc72f82d6837430a08a0823a
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2024-10-15 01:47:11 +0800

    Update rental_management_system.sql

--- backend/database/rental_management_system.sql
+++ backend/database/rental_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2024 at 07:44 PM
+-- Generation Time: Oct 14, 2024 at 07:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 

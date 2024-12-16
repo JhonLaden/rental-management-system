e56ad8749d9ba0c787aa19443075e55f044b28f8
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2024-12-16 23:58:30 +0800

    update

--- backend/database/rental_management_system.sql
+++ backend/database/rental_management_system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2024 at 12:08 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Dec 16, 2024 at 04:57 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,32 +40,47 @@ CREATE TABLE `item` (
   `is_active` tinyint(1) DEFAULT 1,
   `photo` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`item_id`, `name`, `type`, `size`, `deposit_cost`, `rental_cost`, `quantity`, `owner_id`, `in_stock`, `is_active`, `photo`, `description`) VALUES
-(61, 'sdfsd34', 'suit', '234.00', '2342.00', '34.00', 1, 7, 1, 0, '', ''),
-(62, '3sdfsd', 'gown', '34.00', '3232.00', '324.00', 1, 7, 1, 0, '', ''),
-(63, 'asdf', 'gown', '34.00', '23432.00', '234.00', 1, 7, 1, 0, '', ''),
-(64, 'sasf', 'gown', '234.00', '324.00', '32432.00', 1, 7, 1, 0, '', ''),
-(65, '234', 'gown', '234.00', '234.00', '23432.00', 1, 7, 1, 0, '', ''),
-(66, 'Jhon', 'gown', '324.00', '32432.00', '23432.00', 1, 7, 1, 0, '', ''),
-(67, 'Jhon', 'gown', '32.00', '32.00', '324.00', 1, 7, 1, 0, '', ''),
-(68, 'another item', 'gown', '21.00', '23.00', '20.00', 1, 6, 1, 0, '', ''),
-(69, 'Hikoo', 'suit', '32.00', '23.00', '43.00', 1, 6, 0, 0, '', ''),
-(70, 'hgaha', 'suit', NULL, '32.00', '23.00', 0, 7, 1, 0, 'dumb&toopid.png', ''),
-(71, 'White Gown', 'gown', '0.00', '200.00', '250.00', 0, 7, 1, 0, 'gown1.jpeg', 'asdfsdf sf sad'),
-(72, 'black suit', 'suit', NULL, '200.00', '250.00', 0, 7, 1, 0, 'suit3.jpeg', 'sdaf'),
-(73, 'red suit', 'suit', NULL, '250.00', '300.00', 0, 7, 0, 0, 'suit1.jpeg', 'testing description'),
-(74, 'white gown', 'gown', NULL, '200.00', '300.00', 0, 7, 1, 0, 'gown1.jpeg', 'description'),
-(75, 'red suit', 'suit', NULL, '200.00', '300.00', 0, 7, 1, 0, 'suit1.jpeg', 'ahaha'),
-(76, 'red suit', 'suit', NULL, '300.00', '200.00', 0, 7, 1, 1, 'suit1.jpeg', 'haha'),
-(77, 'white gown', 'gown', NULL, '200.00', '300.00', 0, 6, 1, 0, 'gown1.jpeg', 'haha'),
-(78, 'white gown', 'gown', NULL, '200.00', '300.00', 0, 6, 1, 0, 'gown1.jpeg', 'ahaha'),
-(79, 'white gown', 'gown', NULL, '200.00', '300.00', 0, 6, 0, 1, 'gown1.jpeg', 'haha');
+(61, 'sdfsd34', 'suit', 234.00, 2342.00, 34.00, 1, 7, 1, 0, '', ''),
+(62, '3sdfsd', 'gown', 34.00, 3232.00, 324.00, 1, 7, 1, 0, '', ''),
+(63, 'asdf', 'gown', 34.00, 23432.00, 234.00, 1, 7, 1, 0, '', ''),
+(64, 'sasf', 'gown', 234.00, 324.00, 32432.00, 1, 7, 1, 0, '', ''),
+(65, '234', 'gown', 234.00, 234.00, 23432.00, 1, 7, 1, 0, '', ''),
+(66, 'Jhon', 'gown', 324.00, 32432.00, 23432.00, 1, 7, 1, 0, '', ''),
+(67, 'Jhon', 'gown', 32.00, 32.00, 324.00, 1, 7, 1, 0, '', ''),
+(68, 'another item', 'gown', 21.00, 23.00, 20.00, 1, 6, 1, 0, '', ''),
+(69, 'Hikoo', 'suit', 32.00, 23.00, 43.00, 1, 6, 0, 0, '', ''),
+(70, 'hgaha', 'suit', NULL, 32.00, 23.00, 0, 7, 1, 0, 'dumb&toopid.png', ''),
+(71, 'White Gown', 'gown', 0.00, 200.00, 250.00, 0, 7, 1, 0, 'gown1.jpeg', 'asdfsdf sf sad'),
+(72, 'black suit', 'suit', NULL, 200.00, 250.00, 0, 7, 1, 0, 'suit3.jpeg', 'sdaf'),
+(73, 'red suit', 'suit', NULL, 250.00, 300.00, 0, 7, 1, 0, 'suit1.jpeg', 'testing description'),
+(74, 'white gown', 'gown', NULL, 200.00, 300.00, 0, 7, 1, 0, 'gown1.jpeg', 'description'),
+(75, 'red suit', 'suit', NULL, 200.00, 300.00, 0, 7, 1, 0, 'suit1.jpeg', 'ahaha'),
+(76, 'red suit', 'suit', NULL, 300.00, 200.00, 0, 7, 1, 1, 'suit1.jpeg', 'haha'),
+(77, 'white gown', 'gown', NULL, 200.00, 300.00, 0, 6, 1, 0, 'gown1.jpeg', 'haha'),
+(78, 'white gown', 'gown', NULL, 200.00, 300.00, 0, 6, 1, 0, 'gown1.jpeg', 'ahaha'),
+(79, 'white gown', 'gown', NULL, 200.00, 300.00, 0, 6, 1, 0, 'gown1.jpeg', 'haha'),
+(80, 'white gown', 'gown', NULL, 200.00, 300.00, 0, 7, 0, 1, 'gown1.jpeg', 'Size 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `status` enum('unread','read') DEFAULT 'unread',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -84,31 +99,35 @@ CREATE TABLE `rental_schedule` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `cost` decimal(11,2) NOT NULL DEFAULT 0.00,
   `method` varchar(255) DEFAULT NULL,
-  `delivery_address` varchar(255) DEFAULT 'None'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `delivery_address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `rental_schedule`
 --
 
 INSERT INTO `rental_schedule` (`schedule_id`, `start_date`, `return_date`, `borrower_id`, `lender_id`, `item_id`, `status`, `created_at`, `cost`, `method`, `delivery_address`) VALUES
-(82, '2024-12-09', '2024-12-19', 4, 7, 67, 'canceled', '2024-12-08 05:45:26', '3240.00', NULL, 'None'),
-(83, '2024-12-09', '2024-12-10', 4, 7, 64, 'finished', '2024-12-08 05:46:22', '32432.00', NULL, 'None'),
-(84, '2024-12-09', '2024-12-11', 4, 7, 67, 'finished', '2024-12-08 10:55:30', '648.00', NULL, 'None'),
-(85, '2024-12-10', '2024-12-12', 4, 6, 69, 'pending', '2024-12-08 17:33:52', '86.00', NULL, 'None'),
-(86, '2024-12-10', '2024-12-11', 4, 7, 70, 'canceled', '2024-12-08 21:44:03', '23.00', NULL, 'None'),
-(87, '2024-12-10', '2024-12-12', 4, 7, 70, 'canceled', '2024-12-08 21:49:47', '46.00', NULL, 'None'),
-(88, '2024-12-10', '2024-12-11', 4, 7, 73, 'canceled', '2024-12-08 23:25:03', '300.00', NULL, 'None'),
-(89, '2024-12-10', '2024-12-12', 4, 7, 71, 'canceled', '2024-12-09 00:21:32', '500.00', NULL, 'None'),
-(90, '2024-12-10', '2024-12-12', 4, 7, 73, 'canceled', '2024-12-09 00:57:12', '600.00', NULL, 'None'),
-(91, '2024-12-13', '2024-12-17', 4, 7, 73, 'canceled', '2024-12-09 01:23:04', '1200.00', NULL, 'None'),
-(92, '0000-00-00', '0000-00-00', 4, 7, 73, 'canceled', '2024-12-09 03:50:46', '0.00', 'pickup', 'None'),
-(93, '2024-12-10', '2024-12-11', 4, 7, 72, 'canceled', '2024-12-09 04:07:32', '250.00', 'delivery', 'None'),
-(94, '2024-12-09', '2024-12-10', 4, 7, 73, 'canceled', '2024-12-09 04:08:42', '300.00', 'delivery', 'Recodo '),
-(95, '2024-12-09', '2024-12-10', 4, 7, 73, 'canceled', '2024-12-09 04:15:50', '720.00', 'delivery', 'Recodo'),
-(96, '2024-12-09', '2024-12-11', 5, 7, 72, 'canceled', '2024-12-09 04:26:43', '870.00', 'delivery', 'Ayala'),
-(97, '2024-12-09', '2024-12-10', 5, 7, 73, 'pending', '2024-12-09 05:01:21', '550.00', 'pickup', 'None'),
-(98, '2024-12-12', '2024-12-13', 4, 6, 79, 'pending', '2024-12-12 01:13:08', '500.00', 'pickup', 'None');
+(82, '2024-12-09', '2024-12-19', 4, 7, 67, 'canceled', '2024-12-08 05:45:26', 3240.00, NULL, NULL),
+(83, '2024-12-09', '2024-12-10', 4, 7, 64, 'finished', '2024-12-08 05:46:22', 32432.00, NULL, NULL),
+(84, '2024-12-09', '2024-12-11', 4, 7, 67, 'finished', '2024-12-08 10:55:30', 648.00, NULL, NULL),
+(85, '2024-12-10', '2024-12-12', 4, 6, 69, 'canceled', '2024-12-08 17:33:52', 86.00, NULL, NULL),
+(86, '2024-12-10', '2024-12-11', 4, 7, 70, 'canceled', '2024-12-08 21:44:03', 23.00, NULL, NULL),
+(87, '2024-12-10', '2024-12-12', 4, 7, 70, 'canceled', '2024-12-08 21:49:47', 46.00, NULL, NULL),
+(88, '2024-12-10', '2024-12-11', 4, 7, 73, 'canceled', '2024-12-08 23:25:03', 300.00, NULL, NULL),
+(89, '2024-12-10', '2024-12-12', 4, 7, 71, 'canceled', '2024-12-09 00:21:32', 500.00, NULL, NULL),
+(90, '2024-12-10', '2024-12-12', 4, 7, 73, 'canceled', '2024-12-09 00:57:12', 600.00, NULL, NULL),
+(91, '2024-12-13', '2024-12-17', 4, 7, 73, 'canceled', '2024-12-09 01:23:04', 1200.00, NULL, NULL),
+(92, '0000-00-00', '0000-00-00', 4, 7, 73, 'canceled', '2024-12-09 03:50:46', 0.00, 'pickup', NULL),
+(93, '2024-12-10', '2024-12-11', 4, 7, 72, 'canceled', '2024-12-09 04:07:32', 250.00, 'delivery', NULL),
+(94, '2024-12-09', '2024-12-10', 4, 7, 73, 'canceled', '2024-12-09 04:08:42', 300.00, 'delivery', 'Recodo '),
+(95, '2024-12-09', '2024-12-10', 4, 7, 73, 'canceled', '2024-12-09 04:15:50', 720.00, 'delivery', 'Recodo'),
+(96, '2024-12-09', '2024-12-11', 5, 7, 72, 'canceled', '2024-12-09 04:26:43', 870.00, 'delivery', 'Ayala'),
+(97, '2024-12-09', '2024-12-10', 5, 7, 73, 'canceled', '2024-12-09 05:01:21', 550.00, 'pickup', ''),
+(98, '2024-12-16', '2024-12-17', 4, 7, 76, 'rented', '2024-12-16 09:03:33', 500.00, 'pickup', ''),
+(99, '2024-12-16', '2024-12-17', 4, 6, 79, 'canceled', '2024-12-16 09:49:50', 500.00, 'pickup', ''),
+(100, '2024-12-17', '2024-12-19', 4, 7, 80, 'canceled', '2024-12-16 10:06:35', 800.00, 'pickup', ''),
+(101, '2024-12-17', '2024-12-19', 4, 7, 80, 'canceled', '2024-12-16 10:16:33', 800.00, 'pickup', ''),
+(102, '2024-12-16', '2024-12-17', 4, 7, 80, 'pending', '2024-12-16 10:33:33', 500.00, 'pickup', '');
 
 --
 -- Triggers `rental_schedule`
@@ -130,7 +149,7 @@ DELIMITER ;
 
 CREATE TABLE `trigger_debug` (
   `debug_message` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -149,7 +168,7 @@ CREATE TABLE `user` (
   `type` enum('admin','lender','client') DEFAULT 'client',
   `successful_lends` int(11) DEFAULT 0,
   `is_active` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -180,6 +199,13 @@ ALTER TABLE `item`
   ADD KEY `fk_owner` (`owner_id`);
 
 --
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `rental_schedule`
 --
 ALTER TABLE `rental_schedule`
@@ -202,13 +228,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `rental_schedule`
 --
 ALTER TABLE `rental_schedule`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -225,6 +257,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `item`
   ADD CONSTRAINT `fk_owner` FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
 -- Constraints for table `rental_schedule`
